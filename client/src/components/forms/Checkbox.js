c14028378ae05e1ed0c7f6eb3466512c082c4b40
YentelDeHauwere <yentel.dh@hotmail.be>
2021-02-14 16:04:57 +0100

    style checkbox material

--- client/src/components/forms/Checkbox.js
+++ client/src/components/forms/Checkbox.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 
 // Icons
 import Check from '../../assets/icons/check-blue.svg';
+import './Checkbox.scss';
 
 const Checkbox = ({name, id, change}) => {
   const [ checked, setChecked ] = useState();
@@ -11,11 +12,12 @@ const Checkbox = ({name, id, change}) => {
     setChecked(e.target.checked);
   };
 
+
   return (
     <div className="checkbox">
-        {
-          checked && <img src={Check} alt="check" />
-        }
+		{
+			checked && <img src={Check} alt="check" />
+		}
         <input onChange={(e) => changeThis(e)} type="checkbox" name={name} id={id} />
     </div>
   )
